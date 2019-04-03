e412e2ed99550e51bd35565950d5d5f843fb9223
npham014 <npham014@ucr.edu>
2019-04-03 16:57:06 -0700

    Part1-3 Done

---
+++ Lab 2/npham014_lab2_part2/npham014_lab2_part2/main.c
@@ -0,0 +1,24 @@
+/*
+ * npham014_lab2_part2.c
+ *
+ * Created: 4/3/2019 4:06:02 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	unsigned char tempA = 0x00;
+//	unsigned char fullLot = 0x04;
+//Doesn't check the case where input is more than 4.
+	while(1)
+	{
+		tempA = PINA; //tempA is input
+		PORTC = tempA;// Set the input to the output.
+	}
+	return 0;
+}
